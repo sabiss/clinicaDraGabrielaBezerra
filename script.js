9069afcf3ef0ba01ab8bf72bbf07d0f21d302925
sabiss <sabrinabezsilva@gmail.com>
2024-01-14 14:40:23 -0300

    FEAT: script que rola a tela ate as divs de pilares de excelência

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//abre e fecha menu hamburguer
 document.addEventListener("DOMContentLoaded", function () {
   const mobileMenuButton = document.getElementById("mobileMenuButton");
   const mobileMenu = document.getElementById("mobileMenu");
@@ -11,3 +12,15 @@ document.addEventListener("DOMContentLoaded", function () {
     mobileMenu.classList.add("hidden");
   });
 });
+
+//rola a tela caso cliquem em um lin do nav bar
+function rolar(div) {
+  const divParaRolarAte = document.getElementById(div);
+
+  const divParaRolarAteTop = divParaRolarAte.offsetTop;
+
+  window.scrollTo({
+    top: divParaRolarAteTop,
+    behavior: "smooth",
+  });
+}
